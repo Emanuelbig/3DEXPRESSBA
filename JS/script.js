8efe317cd15e4d2708e0ecc80e85a109cef69241
Emanuel Morano <emanueljmoranosole@gmail.com>
2023-05-17 13:57:57 -0300

    se remplateo el trabajo para que sea responsive teniendo como prioridad mobile first

--- JS/script.js
+++
@@ -1,8 +0,0 @@
-let aviso
-aviso = "Esta página está en construcción, sepa entender, disculpe las molestias"
-
-console.log (aviso)
-
-alert (aviso)
-
-// puedo dejar un img en un html aparte para hacer lo de la alerta. 
