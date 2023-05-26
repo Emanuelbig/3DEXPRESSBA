996cfc19044f4c4538b3c7b15aa4149b0dfbd1ef
Emanuel Morano <emanueljmoranosole@gmail.com>
2023-05-26 16:08:26 -0300

    pruebas varias en producto

--- JS/atajoshtml.js
+++ JS/atajoshtml.js
@@ -71,3 +71,58 @@ document.getElementById("main-header").innerHTML=
       </div>
 
   </nav>`
+
+
+document.getElementById("product-header").innerHTML= 
+`<div class="row text-start text-white mt-2 mb-2 ">
+
+</div>
+
+
+
+  <nav class="nav3d">
+      
+      <div class=" bg-primary ">
+
+          <div class="container justify-content-center">
+  
+              <nav class="navbar navbar-expand-lg navbar-dark ">
+  
+                  <div class="container-fluid ">
+  
+                    <a class="navbar-brand" href="index.html" style="font-family: 'Kanit', sans-serif;">3D Express</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav ms-auto mb-0 mb-lg-0 text-start">
+                        
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="empresa.html">Empresa</a>
+                        </li>
+  
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="#services">Servicios</a>
+                        </li>
+  
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="productos.html">Productos</a>
+                        </li>
+  
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="index.html">Contacto</a>
+                        </li>
+
+                        <li class="nav-item">
+                          <a class="nav-link disable" aria-current="page" href="#" disable>Blog</a>
+                        </li>
+                        
+                      </ul>
+                      
+                    </div>
+                  </div>
+                </nav>
+  
+      </div>
+
+  </nav>`
