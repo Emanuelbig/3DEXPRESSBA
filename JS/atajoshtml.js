ecea728773e08f700ec95379f8674afb35b10550
Emanuel Morano <emanueljmoranosole@gmail.com>
2023-05-28 20:15:39 -0300

    pruebas en productos

--- JS/atajoshtml.js
+++ JS/atajoshtml.js
@@ -1,76 +1,82 @@
 // Atajos estructuras HTML
 
-document.getElementById("main-header").innerHTML= 
+document.getElementById("id-header").innerHTML= 
 `<div class="row text-start text-white mt-2 mb-2 ">
 
-    <div class="col-sm-12 col-md-4 col-lg-3 col-xl-3">
+<div class="col-sm-12 col-md-4 col-lg-3 col-xl-3">
 
-      <a href="" target="_blank" class="iconcontact">
-        <img src="IMG/call.svg" alt="call" class="iconcontactreal">
-        <p class="contactinfo">+54 9 11 3316 8094</p>
-      </a>
+  <a href="" target="_blank" class="iconcontact">
+    <svg fill="currentcolor" alt="call" class="iconcontactreal" width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M4.59375 7.78125C6.09375 10.6875 8.29688 12.8906 11.2031 14.3906L13.4062 12.1875C13.7188 11.875 14.0625 11.7969 14.4375 11.9531C15.5625 12.3281 16.75 12.5156 18 12.5156C18.2812 12.5156 18.5156 12.6094 18.7031 12.7969C18.8906 12.9844 18.9844 13.2188 18.9844 13.5V17.0156C18.9844 17.2969 18.8906 17.5312 18.7031 17.7188C18.5156 17.9062 18.2812 18 18 18C13.3125 18 9.29688 16.3438 5.95312 13.0312C2.64062 9.6875 0.984375 5.67188 0.984375 0.984375C0.984375 0.703125 1.07812 0.46875 1.26562 0.28125C1.45312 0.09375 1.6875 0 1.96875 0H5.48438C5.76562 0 6 0.09375 6.1875 0.28125C6.375 0.46875 6.46875 0.703125 6.46875 0.984375C6.46875 2.23438 6.65625 3.42188 7.03125 4.54688C7.15625 4.95312 7.07812 5.29688 6.79688 5.57812L4.59375 7.78125Z" fill="white"/>
+    </svg>
+    <!-- <img src="IMG/call.svg" alt="call" class="iconcontactreal"> -->
+    <p class="contactinfo">+54 9 11 3316 8094</p>
+  </a>
 
-    </div>
+</div>
 
-    <div class="col-sm-12 col-md-4 col-lg-3 col-xl-3">
+<div class="col-sm-12 col-md-4 col-lg-3 col-xl-3">
 
-      <a href="" target="_blank" class="iconcontact">
-        <img src="IMG/email.svg" alt="email">
-        <p class="contactinfo">3dexpress.ba@gmail.com</p>
-      </a>   
+  <a href="" target="_blank" class="iconcontact">
+    <svg fill="currentcolor" alt="email" class="iconcontactreal" width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M18 5.01562V3L9.98438 8.01562L1.96875 3V5.01562L9.98438 9.98438L18 5.01562ZM18 0.984375C18.5312 0.984375 18.9844 1.1875 19.3594 1.59375C19.7656 2 19.9688 2.46875 19.9688 3V15C19.9688 15.5312 19.7656 16 19.3594 16.4062C18.9844 16.8125 18.5312 17.0156 18 17.0156H1.96875C1.4375 17.0156 0.96875 16.8125 0.5625 16.4062C0.1875 16 0 15.5312 0 15V3C0 2.46875 0.1875 2 0.5625 1.59375C0.96875 1.1875 1.4375 0.984375 1.96875 0.984375H18Z" fill="white"/>
+    </svg>
+    <!-- <img src="IMG/email.svg" alt="email"> -->
+    <p class="contactinfo">3dexpress.ba@gmail.com</p>
+  </a>   
 
-    </div>
+</div>
 
 </div>
 
 
 
-  <nav class="nav3d">
-      
-      <div class=" bg-primary ">
+<nav class="nav3d">
+  
+  <div class=" bg-primary ">
 
-          <div class="container justify-content-center">
-  
-              <nav class="navbar navbar-expand-lg navbar-dark ">
-  
-                  <div class="container-fluid ">
-  
-                    <a class="navbar-brand" href="index.html" style="font-family: 'Kanit', sans-serif;">3D Express</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                      <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                      <ul class="navbar-nav ms-auto mb-0 mb-lg-0 text-start">
-                        
-                        <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="empresa.html">Empresa</a>
-                        </li>
-  
-                        <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="#services">Servicios</a>
-                        </li>
-  
-                        <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="productos.html">Productos</a>
-                        </li>
-  
-                        <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="index.html">Contacto</a>
-                        </li>
+      <div class="container justify-content-center">
 
-                        <li class="nav-item">
-                          <a class="nav-link disable" aria-current="page" href="#" disable>Blog</a>
-                        </li>
-                        
-                      </ul>
-                      
-                    </div>
-                  </div>
-                </nav>
-  
-      </div>
+          <nav class="navbar navbar-expand-lg navbar-dark ">
 
-  </nav>`
+              <div class="container-fluid ">
+
+                <a class="navbar-brand" href="index.html" style="font-family: 'Kanit', sans-serif;">3D Express</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav ms-auto mb-0 mb-lg-0 text-start">
+                    
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="empresa.html">Empresa</a>
+                    </li>
+
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="#services">Servicios</a>
+                    </li>
+
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="productos.html">Productos</a>
+                    </li>
+
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="index.html">Contacto</a>
+                    </li>
+
+                    <li class="nav-item">
+                      <a class="nav-link disable" aria-current="page" href="#" disable>Blog</a>
+                    </li>
+                    
+                  </ul>
+                  
+                </div>
+              </div>
+            </nav>
+
+  </div>
+
+</nav>`
 
 
 document.getElementById("product-header").innerHTML= 
