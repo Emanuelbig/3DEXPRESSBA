c55615b711b194658ad18642db478b25d55ba881
Emanuel Morano <emanueljmoranosole@gmail.com>
2023-05-30 15:39:05 -0300

    video en productos

--- JS/script.js
+++ JS/script.js
@@ -15,3 +15,23 @@ function pay() {
     // console.log(product, price);
     window.alert(products.join(", \n"));
 }
+
+
+//JS comportamiento video
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    var video = document.getElementById('video1');
+    var texto = document.getElementById('textovideo');
+    
+    video.addEventListener('play', function() {
+        video.classList.add('playing');
+        texto.style.opacity = 0;
+        texto.style.pointerEvents = 'none';
+    });
+    
+    video.addEventListener('pause', function() {
+        video.classList.remove('playing');
+        texto.style.opacity = 1;
+        texto.style.pointerEvents = 'auto';
+    });
+});
